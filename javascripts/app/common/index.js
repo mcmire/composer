ada4534eb6e6fc4abd16b1b61959157eb2c5a726
Elliot Winkler <elliot.winkler@gmail.com>
2012-12-04 23:20:05 -0700

    add common

---
+++ javascripts/app/common/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+;(function () {
+  var TRACKS = ['snare', 'kick']
+    , NUMBER_OF_TICKS = 16
+    , TICK_DURATION_IN_BEATS = 1 / NUMBER_OF_TICKS
+    , NUMBER_OF_ITERATIONS = 4
+
+    , composer = {
+        tracks: TRACKS
+      , numberOfTicks: NUMBER_OF_TICKS
+      }
+
+  if (typeof module === 'undefined') {
+    window.composer || (window.composer = {})
+    $.v.extend(window.composer, composer)
+  } else {
+    module.exports = composer
+  }
+})()
